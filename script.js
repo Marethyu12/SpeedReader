15ade2653da1d72daa9d1bdbb42faeef4bb152d4
Marethyu12 <42195546+Marethyu12@users.noreply.github.com>
2019-11-06 12:03:15 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+var words;
+var index;
+var intrvl;
+var paused;
+var delay;
+
+function processText() {
+	words = $("#textfield").val().split(" ");
+	words.forEach((word, index) => words[index] = word.replace(/,\s*$/, ""));
+}
+
+function play() {
+	index = 0;
+	
+	intrvl = setInterval(function() {
+		if (!paused) {
+			if (index == words.length) {
+				return;
+			}
+			
+			$("#word").val(words[index++]);
+		}
+	}, delay);
+}
+
+function pause() {
+	paused = true;
+}
+
+function stop() {
+	clearInterval(intrvl);
+}
+
+/**/
+
+function onLoad1() {
+    $("#spreed").click(() => {
+        words = $("textarea").val().split(" ");
+        open("spreeder.html", "_self");
+    });
+}
+
+function onLoad2() {
+    $("#play").click(() => {
+        console.log("foo");
+    });
+    
+    $("#pause").click(() => {
+        console.log("bar");
+    });
+    
+    $("#slider").attr("min", "0");
+    $("#slider").attr("max", words.length.toString());
+    
+    document.getElementById("slider").addEventListener("change", () => {
+        console.log(this.value);
+    });
+}
