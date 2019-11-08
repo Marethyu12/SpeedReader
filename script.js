1f43530b7d2313e94d5e35129380cb49a322ff5f
Marethyu12 <42195546+Marethyu12@users.noreply.github.com>
2019-11-08 11:32:50 -0800

    Update

--- script.js
+++ script.js
@@ -1,62 +1,58 @@
-var words;
-var index;
-var intrvl;
-var paused;
-var delay;
-
-function processText() {
-	words = $("#textfield").val().split(" ");
-	words.forEach((word, index) => words[index] = word.replace(/,\s*$/, ""));
-}
-
-function play() {
-	index = 0;
-	
-	intrvl = setInterval(function() {
-		if (!paused) {
-			if (index == words.length) {
-				return;
-			}
-			
-			$("#word").val(words[index++]);
-		}
-	}, delay);
-}
-
-function pause() {
-	paused = true;
-}
-
-function stop() {
-	clearInterval(intrvl);
-}
-
-/**/
-
 function onLoad1() {
     $("#spreed").click(() => {
-        localStorage.setItem("words", $("textarea").val().split(" "));//
-        //words = $("textarea").val().split(" ");
-        console.log(words);
+        localStorage.setItem("wpm", parseInt($("#textfield").val()));
+        localStorage.setItem("txt", $("textarea").val());
         open("spreeder.html", "_self");
     });
 }
 
 function onLoad2() {
-    words = localStorage.getItem("words");
+    var wpm = localStorage.getItem("wpm");
+    var words = localStorage.getItem("txt").split(" ");
+    
+    var index = -1;
+    var delay = 60000 / wpm;
+    var intrvl;
+    var intrvlRunning = false;
+    var paused = false;
     
     $("#play").click(() => {
-        console.log("foo");
+        if (!intrvlRunning) {
+            intrvlRunning = true;
+            
+            intrvl = setInterval(() => {
+                if (paused) {
+                    return;
+                }
+                
+                if (index == words.length) {
+                    index = -1;
+                    intrvlRunning = false;
+                    clearInterval(intrvl);
+                }
+                
+                ++index;
+                
+                $("#slider").val(index);
+                $("#slider").trigger("change");
+                $("#box").html(words[index]);
+            }, delay);
+        }
+        
+        if (paused) {
+            paused = false;
+        }
     });
     
     $("#pause").click(() => {
-        console.log("bar");
+        paused = true;
     });
     
     $("#slider").attr("min", "0");
     $("#slider").attr("max", words.length.toString());
-    
-    document.getElementById("slider").addEventListener("change", () => {
-        console.log(this.value);
+    $("#slider").on("input", () => {
+        paused = true;
+        index = parseInt($("#slider").val());
+        $("#box").html(words[index]);
     });
 }
